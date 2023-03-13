62a692f3aec1bf70f1230f1ffae767b30082e3d2
bwlasichuk1 <bwlasichuk1@WA312-10>
2023-03-13 16:32:09 -0600

    It works

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    Rigidbody2D rBody;
+    PlayerInput playerInput;
+    Vector2 moveVector;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
+        moveVector = Vector2.zero;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rBody.AddForce(14.5f * moveVector * Vector2.right);
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.action.WasPerformedThisFrame())
+        {
+            rBody.AddForce(8f * Vector2.up, ForceMode2D.Impulse);
+        }
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveVector = context.ReadValue<Vector2>();
     }
 }
