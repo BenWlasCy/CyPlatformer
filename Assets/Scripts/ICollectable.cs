5197edb6c4a3031f1da22898d7df35226d8759ff
Benjamin Wlasichuk <bwlasichuk1@nait.ca>
2023-03-16 09:46:39 -0600

    Thursday Push

---
+++ Assets/Scripts/ICollectable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICollectable
+{
+    public int Value { get; }
+    public int Collect();
+    public CollectableType Type { get; }
+}
+public enum CollectableType
+{
+    None = 0,
+    Money = 1,
+    Key = 2,
+    Gem = 4,
+}
